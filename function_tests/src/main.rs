ed59f8f4c625030ecf88d0fe3d2b4feea75b5ad2
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2022-10-05 00:55:38 -0700

    added some values to my variables

--- function_tests/src/main.rs
+++ function_tests/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("damn.");
-    new_function();
+    new_function(5);
 }
 
-fn new_function() { 
-    println!("isn't code that executes not-top to bottom kinda weird to you, or is that just me?");
+fn new_function(x:u64) { 
+    println!("isn't code that executes not-top to bottom kinda weird to you, or is that just me?, also here's an variable: {x}");
 }
