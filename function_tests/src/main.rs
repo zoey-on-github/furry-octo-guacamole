f674e1e9dd5c9e7ba1509f4e394d0ffff8572567
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2022-10-30 21:33:16 -0700

    added some more things to my function test added another variable

--- function_tests/src/main.rs
+++ function_tests/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("damn.");
-    new_function(5);
+    new_function(5, "f");
 }
 
-fn new_function(x:u64) { 
-    println!("isn't code that executes not-top to bottom kinda weird to you, or is that just me?, also here's an variable: {x}");
+fn new_function(y:u64,f:char) { 
+    println!("isn't code that executes not-top to bottom kinda weird to you, or is that just me?, also here's an variable: {y}, {f}");
 }
---
+++ function_tests/target/debug/deps/function_tests-f4c73d2ad5ac8b5b.d
@@ -0,0 +1,5 @@
+/home/julie/furry-octo-guacamole/function_tests/target/debug/deps/function_tests-f4c73d2ad5ac8b5b: src/main.rs
+
+/home/julie/furry-octo-guacamole/function_tests/target/debug/deps/function_tests-f4c73d2ad5ac8b5b.d: src/main.rs
+
+src/main.rs:
