c298c2fe8f7fca4f3661d73fac26375c9986a908
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2022-10-30 21:38:17 -0700

    added another variable

--- function_tests/src/main.rs
+++ function_tests/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("damn.");
-    new_function(5, "f");
+    new_function(5, 'f');
 }
 
 fn new_function(y:u64,f:char) { 
-    println!("isn't code that executes not-top to bottom kinda weird to you, or is that just me?, also here's an variable: {y}, {f}");
+    println!("isn't code that executes not-top to bottom kinda weird to you, or is that just me?, also here's an variable: {y}, here's another: {f}");
 }
