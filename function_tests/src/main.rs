572aaa44ca8a47643c777cdf55b630bbdcb99646
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2022-10-30 22:51:07 -0700

    added a few more functions

--- function_tests/src/main.rs
+++ function_tests/src/main.rs
@@ -1,8 +1,23 @@
 fn main() {
     println!("damn.");
     new_function(5, 'f');
+    print()
 }
 
 fn new_function(y:u64,f:char) { 
     println!("isn't code that executes not-top to bottom kinda weird to you, or is that just me?, also here's an variable: {y}, here's another: {f}");
+    let z = {
+        let e = 4;
+        e + 2
+    };
+    println!("{z}")
+}
+
+fn five() -> u64 {
+    5
+}
+
+fn print() {
+    let s = five();
+    println!("{s} is the number after 4");
 }
