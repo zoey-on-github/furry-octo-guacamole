f6efd39971a2fa702052037956797290ebc58443
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2023-01-10 22:19:21 -0800

    added even more control flow stuff, just basic if/else stuff

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -8,7 +8,11 @@ fn main() {
     }
 
     let other_number = 4;
-        if other_number != 3 {
-            println!("number");
+        if other_number % 3 == 0 {
+            println!("number is disivible by 3");
+        } else if other_number % 4 == 0 {
+            println!("number is divisble by 4");
+        } else if other_number % 6 == 0 {
+            println!("number is divisble by 6");
         }
 }
