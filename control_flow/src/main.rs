2b4a55b17eaa8dce08bc36c092414f2b0aec7794
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2023-03-23 21:52:01 -0700

    jfioew

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,18 +1,4 @@
 fn main() {
-    let conditional_number = 6; 
+    let mut counter = 0;
 
-    if conditional_number < 5 { 
-        println!("condition was met");
-    } else {
-        println!("condition was not met");
-    }
-
-    let other_number = 4;
-        if other_number % 3 == 0 {
-            println!("number is disivible by 3");
-        } else if other_number % 4 == 0 {
-            println!("number is divisble by 4");
-        } else if other_number % 6 == 0 {
-            println!("number is divisble by 6");
-        }
 }
